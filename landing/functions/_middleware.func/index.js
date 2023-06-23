123b51f786ecb6092fe30d4d8a6a73ac98c24406
Andy Bitz <artzbitz@gmail.com>
2023-06-23 17:50:24 +0200

    Complete middleware

--- landing/functions/_middleware.func/index.js
+++ landing/functions/_middleware.func/index.js
@@ -1,12 +1,39 @@
 export default function middleware(request, _event) {
-	console.log('request.host', request.host);
-	console.log('request.url', request.url);
+	const url = new URL(request.url);
+
+	const redirect = url.searchParams.get('game');
+	if (redirect) {
+		// Redirect to the correct host
+		const dest = new URL(`${url.protocol}//${redirect}.${url.host}${url.pathname}${url.search}`);
+
+		return new Response(null, {
+			status: 307,
+			headers: {
+				Location: String(dest),
+			}
+		});
+	}
+
+	const game = url.host.split('.')[0];
+
+	const games = new Set([
+		'anims',
+		'phaser-first-test',
+		'states',
+	]);
+
+	if (games.has(game)) {
+		// Rewrite to the game by prefixing the path for the rewrite
+		const dest = new URL(`/${game}/${url.pathname}${url.search}`, url);
+
+		const response = new Response();
+		response.headers.set('x-middleware-rewrite', String(dest));
+
+		return response;
+	}
 
 	// Do nothing
-	const headers = new Headers(request.headers ?? {});
-	headers.set('x-middleware-next', '1');
-
-	return new Response(null, {
-		headers,
-	});
+	const response = new Response();
+	response.headers.set('x-middleware-next', '1');
+	return response;
 }
