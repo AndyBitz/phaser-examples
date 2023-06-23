ac1f3fe9f8f02519e9a56c65052741734ea10cc5
Andy Bitz <artzbitz@gmail.com>
2023-06-23 18:04:54 +0200

    Use cookies

--- landing/functions/_middleware.func/index.js
+++ landing/functions/_middleware.func/index.js
@@ -1,26 +1,39 @@
 export default function middleware(request, _event) {
 	const url = new URL(request.url);
 
-	const redirect = url.searchParams.get('game');
-	if (redirect) {
-		// Redirect to the correct host
-		const dest = new URL(`${url.protocol}//${redirect}.${url.host}${url.pathname}${url.search}`);
+	// This is an unnecessarily complex way to make this work,
+	// but better than rewriting all the games.
+	//
+	// When a request comes in on the root path it'll
+	// set a cookie when `game` is set so that all follow up requests
+	// will be rewritten to that game.
+	//
+	// If then another request to the root games in without the
+	// query param, it'll unset the cookie again.
 
+	const gameInit = url.searchParams.get('game');
+	if (gameInit && url.pathname === '/') {
 		return new Response(null, {
-			status: 307,
 			headers: {
 				Location: String(dest),
-			}
+				'x-middleware-next': '1',
+				'Set-Cookie': `game=${gameInit}; Path=/; HttpOnly`,
+			},
 		});
 	}
-
-	const game = url.host.split('.')[0];
 
 	const games = new Set([
 		'anims',
 		'phaser-first-test',
 		'states',
 	]);
+
+	const cookies = new Map(request.headers.get('cookie')?.split(';').map(raw => {
+		const [name, value] = raw.split('=');
+		return [name.trimLeft(), value]
+	}) || []);
+
+	const game = cookies.get('game');
 
 	if (games.has(game)) {
 		// Rewrite to the game by prefixing the path for the rewrite
@@ -35,5 +48,10 @@ export default function middleware(request, _event) {
 	// Do nothing
 	const response = new Response();
 	response.headers.set('x-middleware-next', '1');
+
+	if (!gameInit && url.pathname === '/' && cookies.get('game')) {
+		response.headers.set('Set-Cookie', `game=; Path=/; HttpOnly; Max-Age=0`);
+	}
+
 	return response;
 }
